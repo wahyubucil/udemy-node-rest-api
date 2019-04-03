02c4dc69b451b41ec095fc21421b68e5c4aed552
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-04-03 21:11:42 +0800

    Create the get method

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express')
 const app = express()
 
+const genres = [
+  { id: 1, name: 'Action' },
+  { id: 2, name: 'Drama' },
+  { id: 3, name: 'Romance' }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/api/genres', (req, res) => {
+  res.send(genres)
+})
+
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id))
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.')
+
+  res.send(genre)
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
